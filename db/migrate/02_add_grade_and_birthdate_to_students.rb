11577a65df77c4417da1793f8a3b6205c00a6237
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-27 13:22:15 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
 
   def change
-    add column :students, :grade, :integer
-    add column :students, :birthdate, :string
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
   end
 end
